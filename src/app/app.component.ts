b0758b835f2a2710d841b91f782d89b89a39175b
Desock <salvagomezrisen@gmail.com>
2024-12-18 18:29:02 +0100

    Pequeños aportes a la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { relojComponent } from './reloj/components/reloj_component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ud09-act01-p2';
+  title = 'GESTIÓN DEL BANCO';
+  cantidad = '150000,43 €'
+  
 }
